51fb3a0ce84f22f4f3c3ccdfad4eef25def6089c
Nada Nader Elgeziry <38576376+nadaelgeziry@users.noreply.github.com>
2018-06-04 13:11:47 +0200

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('scss', function(){
+	return gulp.src('src/scss/base.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function(){
+	return gulp.watch('src/scss/*.scss',['scss']);
+});
+
+gulp.task('default',['scss','watch']);
